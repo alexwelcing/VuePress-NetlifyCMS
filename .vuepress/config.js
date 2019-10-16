14d60fad806f96f4ce146a20202fb3525a26fb62
Alex Welcing <alexwelcing@gmail.com>
2019-10-15 20:16:42 -0400

    Update config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: 'VuePress-NetlifyCMS',
-  description: 'A VuePress Site Deployed on Netlify.com by Demo Macro',
+  title: 'Atrispina Desert Vues',
+  description: 'Those are not rain clouds.',
   dest: 'pages',
   themeConfig: {
 
@@ -9,13 +9,11 @@ module.exports = {
       {
         text: 'Projects',
         items: [
-          { text: 'URLStack', link: 'https://abc.imst.xyz' },
-          { text: 'Hexo-NetlifyCMS', link: 'https://hexocms.imst.xyz' },
-          { text: 'Gitbook-NetlifyCMS', link: 'https://gitbook.imst.xyz' }
+          { text: 'Ghost Location', link: 'https://atrispina.stream' },
+          { text: 'Gatsby fed by Ghost', link: 'https://atrispina.com' }
         ]
       },
-      { text: 'About', link: 'about.md' },
-      { text: 'Admin', link: 'https://vuepress.imst.xyz/admin/' }
+      { text: 'About', link: 'about.md' }
     ],
 
     sidebar: 'auto',
@@ -24,37 +22,11 @@ module.exports = {
       ['docs/', 'Documents']
     ],
 
-    displayAllHeaders: true, // 默认值：false
-    activeHeaderLinks: true, // 默认值：true
-    lastUpdated: 'Last Updated', // string | boolean
-
+    displayAllHeaders: true, 
+    activeHeaderLinks: true, 
+    lastUpdated: 'Last Updated', 
     serviceWorker: {
-      updatePopup: true // Boolean | Object, 默认值是 undefined.
-      // 如果设置为 true, 默认的文本配置将是: 
-      // updatePopup: { 
-      //    message: "New content is available.", 
-      //    buttonText: "Refresh" 
-      // }
+      updatePopup: true }
     },
-
-
-    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: 'DemoMacro/VuePress-NetlifyCMS',
-    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    repoLabel: 'Github',
-
-    // 以下为可选的编辑链接选项
-
-    // 假如你的文档仓库和项目本身不在一个仓库：
-    // docsRepo: 'DemoMacro/VueCMS',
-    // 假如文档不是放在仓库的根目录下：
-    // docsDir: 'docs',
-    // 假如文档放在一个特定的分支下：
-    docsBranch: 'master',
-    // 默认是 false, 设置为 true 来启用
-    editLinks: true,
-    // 默认为 "Edit this page"
-    editLinkText: '帮助我们改善此页面！'
   }
 }
